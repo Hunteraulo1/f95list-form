f53f09556e5ec9274128b3a49e43e3fc49778972
Hunteraulo1 <hunteraulo@pm.me>
2024-10-29 16:58:41 +0100

    :rotating_light: biome linting

--- src/components/Panel.svelte
+++ src/components/Panel.svelte
@@ -10,9 +10,13 @@ export let showDivider = true;
     <h3 class="card-title">{title}</h3>
     <slot name="button" />
   </div>
-  <div class="card mt-4 rounded-md border-2 border-base-300 bg-base-100 p-8 shadow-sm">
+  <div
+    class="card mt-4 rounded-md border-2 border-base-300 bg-base-100 p-8 shadow-sm"
+  >
     {#if $isLoading}
-      <div class="absolute left-0 top-0 h-full w-full bg-base-300 opacity-50"></div>
+      <div
+        class="absolute left-0 top-0 h-full w-full bg-base-300 opacity-50"
+      ></div>
     {/if}
     <slot class="text-base-200" name="description" />
     {#if showDivider}
