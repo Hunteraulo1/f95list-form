bbdaca35fdd89a6accf9774e2d94fc2f8fda5684
Hunteraulo1 <hunteraulo@pm.me>
2024-11-20 06:15:34 +0100

    :coffin: remove dead prop

--- src/routes/protected/AddGame.svelte
+++ src/routes/protected/AddGame.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 import FormGame from '$components/FormGame.svelte';
-import checkUser from '$lib/checkUser';
 </script>
 
-<FormGame isAdmin={checkUser(['admin'])} />
+<FormGame />
  
