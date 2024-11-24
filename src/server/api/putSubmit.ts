7600a99965d9675b0bfdcd498f764d8f9e040a8a
Hunteraulo1 <hunteraulo@pm.me>
2024-11-24 13:09:05 +0100

    :bug: confirm before submit

--- src/server/api/putSubmit.ts
+++ src/server/api/putSubmit.ts
@@ -39,16 +39,27 @@ export const putSubmit = async ({ submit, type }: PutSubmitArgs): Promise<void> 
 
   if (type !== 'validated' && type !== 'rejected') throw new Error('invalid type');
 
+  let confirmed: boolean | undefined;
+
   const result = submits.map((s) => {
-    if (s.query !== submit.query) return s;
+    if (
+      s.query?.id === submit.query?.id &&
+      s.query?.name === submit.query?.name &&
+      s.query?.version === submit.query?.version
+    )
+      return s;
 
     s.status = type;
     s.reason = submit.reason;
+
+    confirmed = true;
 
     if (type === 'validated') s.game = submit.game;
 
     return s;
   });
+
+  if (!confirmed) throw new Error('Submit not found');
 
   const scriptPropertiesService = PropertiesService.getScriptProperties();
 
