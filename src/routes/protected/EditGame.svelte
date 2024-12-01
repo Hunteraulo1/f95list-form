b288d52c8ab917f902beb3d48c47b1349b1e8710
Hunteraulo1 <hunteraulo@pm.me>
2024-12-01 17:07:37 +0100

    :bug: fix search on edit page

--- src/routes/protected/EditGame.svelte
+++ src/routes/protected/EditGame.svelte
@@ -1,25 +1,17 @@
 <script lang="ts">
-import { onMount } from 'svelte';
-import { navigate } from 'svelte-routing';
-
 import FormGame from '$components/FormGame.svelte';
 import { GAS_API } from '$lib/GAS_API';
 import { isLoading, queryGame } from '$lib/stores';
-import type { GameType } from '$types/schemas';
+import type { GameType, QueryGameType } from '$types/schemas';
+import { navigate } from 'svelte-routing';
 
 let game: GameType | null = $state(null);
 
-const fetchGame = async (): Promise<void> => {
-  const query = $queryGame;
-
-  if (!query) return navigate('/');
-
+const fetchGame = async (query: QueryGameType): Promise<void> => {
   console.info(`Fetching results for query: ${query.name} ${query.version}`);
 
   $isLoading = true;
   game = null;
-
-  if (!query) return navigate('/');
 
   try {
     const result = await GAS_API.getGame(query);
@@ -34,11 +26,13 @@ const fetchGame = async (): Promise<void> => {
   }
 };
 
-onMount(() => {
-  fetchGame();
+$effect(() => {
+  if (!$queryGame) return navigate('/');
+
+  fetchGame($queryGame);
 });
 </script>
 
 {#if game}
-  <FormGame bind:game step={5} edit />
+  <FormGame {game} step={5} edit />
 {/if}
