0dbda4fc644639a35b8822ad8ebb5df6dd6966e9
Hunteraulo1 <hunteraulo@pm.me>
2024-12-10 09:38:19 +0100

    :seedling: change mock addConfig data

--- src/lib/mocks/data/appConfiguration.ts
+++ src/lib/mocks/data/appConfiguration.ts
@@ -3,4 +3,9 @@ import { user } from './user';
 export const appConfiguration = {
   appName: 'Mock App',
   deployingUserEmail: user.email,
+  webhooks: {
+    update: '',
+    logs: '',
+    traductor: '',
+  },
 };
