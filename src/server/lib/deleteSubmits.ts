971cd4295fe1eeb697ff2be4710453aeae871e8f
Hunteraulo1 <hunteraulo@pm.me>
2024-12-18 17:18:40 +0100

    :sparkles: auto delete old submits

---
+++ src/server/lib/deleteSubmits.ts
@@ -0,0 +1,30 @@
+import { getSubmits } from '../api/getSubmits';
+
+import type { SubmitType } from '$types/schemas';
+
+const deleteSubmits = async (): Promise<void> => {
+  console.info('deleteSubmits');
+
+  const submits = await getSubmits({});
+  const newSubmits: SubmitType[] = [];
+
+  if (!submits) throw new Error('postSubmit ~ Submits not found');
+  const dateNow = Date.now();
+
+  for (const s of submits) {
+    if ((s.status === 'rejected' || s.status === 'validated') && new Date(s.date).getTime() + 86_400_000 * 7 >= dateNow)
+      continue;
+
+    newSubmits.push(s);
+  }
+
+  try {
+    const scriptPropertiesService = PropertiesService.getScriptProperties();
+
+    scriptPropertiesService.setProperty('submits', JSON.stringify(newSubmits));
+  } catch (error) {
+    console.error(error);
+
+    throw new Error('postSubmit ~ Un problème est survenue lors de soumission du jeu');
+  }
+};
